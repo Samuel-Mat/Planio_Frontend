bb1f111df31fa525de92cf1c5a5ccc416f18d1ac
S.Matarese.inf21 <s.matarese.inf21@stud.bbbaden.ch>
2024-03-19 13:18:43 +0100

    Initial Commit

---
+++ js/prompthandler.js
@@ -0,0 +1,36 @@
+let userRole = "teacher";
+
+function ShowAddPrompt() {
+  const addPrompt = document.getElementById("addPrompt");
+  addPrompt.style.visibility = "Visible";
+
+  if (userRole == "teacher") {
+  }
+}
+
+function ShowAddRoom() {
+  CloseAdd();
+
+  const addRoomPrompt = document.getElementById("addRoom");
+  addRoomPrompt.style.visibility = "Visible";
+}
+
+function ShowAddPerson() {
+  CloseAdd();
+
+  const addRoomPrompt = document.getElementById("addPerson");
+  addRoomPrompt.style.visibility = "Visible";
+}
+
+function CloseAdd() {
+  const addPrompt = document.getElementById("addPrompt");
+  addPrompt.style.visibility = "Hidden";
+}
+
+function CloseAddAll() {
+  const addPrompts = document.getElementsByClassName("prompt");
+
+  for (let i = 0; i < addPrompts.length; i++) {
+    addPrompts[i].style.visibility = "Hidden";
+  }
+}
